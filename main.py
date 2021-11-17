b903da5c01ec6fa137adfb7ba095b5c66c674ae4
Sriram-bb63 <invadersriram2002@gmail.com>
2021-11-17 22:20:51 +0530

    push all files

---
+++ main.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import psutil
+import time
+
+def temp_logger(temperature_package_id, temperature_core_0, temperature_core_1):
+	d = psutil.sensors_temperatures()
+	t1 = d["coretemp"][0][1]
+	t2 = d["coretemp"][1][1]
+	t3 = d["coretemp"][2][1]
+	temperature_package_id.append(t1)
+	temperature_core_0.append(t2)
+	temperature_core_1.append(t3)
+	print("Logged")
+	time.sleep(5)
+
+print(f"[INFO] Start {time.ctime()}")
+run_time = []
+run_time.append(time.ctime())
+global temperature_package_id
+global temperature_core_0
+global temperature_core_1
+temperature_package_id = []
+temperature_core_0 = []
+temperature_core_1 = []
+print("[INFO] Press Ctrl+c to stop")
+while True:
+	try:
+		temp_logger(temperature_package_id, temperature_core_0, temperature_core_1)
+	except KeyboardInterrupt:
+		print(f"[INFO] Stop {time.ctime()}")
+		break
+run_time.append(time.ctime()) 
+
+df = pd.DataFrame(
+	{
+		"package_id": temperature_package_id,
+		"core_0": temperature_core_0,
+		"core_1": temperature_core_1
+	}
+)
+df.to_csv("temp_log.csv")
+print("[INFO] Data saved as temp_log.csv")
+
+df = pd.read_csv("temp_log.csv")
+df.drop()
