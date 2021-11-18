72888a885d43f9d27357a9e121aae362c47efb45
Sriram-bb63 <invadersriram2002@gmail.com>
2021-11-18 17:06:34 +0530

    add time col

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pandas as pd
 import psutil
 import time
 
-def temp_logger(temperature_package_id, temperature_core_0, temperature_core_1):
+def temp_logger():
 	d = psutil.sensors_temperatures()
 	t1 = d["coretemp"][0][1]
 	t2 = d["coretemp"][1][1]
 	t3 = d["coretemp"][2][1]
+	time_lst.append(time.ctime())
 	temperature_package_id.append(t1)
 	temperature_core_0.append(t2)
 	temperature_core_1.append(t3)
@@ -14,11 +15,11 @@ def temp_logger(temperature_package_id, temperature_core_0, temperature_core_1):
 	time.sleep(2)
 
 print(f"[INFO] Start {time.ctime()}")
-run_time=[]
-run_time.append(time.ctime())
+global time_lst
 global temperature_package_id
 global temperature_core_0
 global temperature_core_1
+time_lst = []
 temperature_package_id = []
 temperature_core_0 = []
 temperature_core_1 = []
@@ -29,14 +30,14 @@ while True:
 	except KeyboardInterrupt:
 		print(f"\n[INFO] Stop {time.ctime()}")
 		break
-run_time.append(time.ctime()) 
-
 df = pd.DataFrame(
 	{
+		"time": time_lst,
 		"package_id": temperature_package_id,
 		"core_0": temperature_core_0,
 		"core_1": temperature_core_1
 	}
 )
+print(f"Start: {time_lst[0]}	Stop: {time_lst[-1]}")
 df.to_csv("temp_log.csv")
 print("[INFO] Data saved as temp_log.csv")
