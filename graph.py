5d1c1a80f2d5906c719b7e3902e1b6e9ce27dde2
Sriram-bb63 <invadersriram2002@gmail.com>
2021-11-23 18:44:53 +0530

    better live graph with core temps

--- graph.py
+++ graph.py
@@ -5,17 +5,28 @@ import pandas as pd
 df = pd.read_csv("temp_log.csv")
 
 x = iter(df["time"])
-y = iter(df["package_id"])
+y_package_id = iter(df["package_id"])
+y_core_0 = iter(df["core_0"])
+y_core_1 = iter(df["core_1"])
+# y_ram = iter(df["ram"])
+# y_fan = iter(df["fan"])
 
 x_plt = []
-y_plt = []
+y_package_id_plt = []
+y_core_0_plt = []
+y_core_1_plt = []
 
 def animate(i):
     x_plt.append(next(x))
-    y_plt.append(next(y))
+    y_package_id_plt.append(next(y_package_id))
+    y_core_0_plt.append(next(y_core_0))
+    y_core_1_plt.append(next(y_core_1))
     plt.cla()
     plt.grid()
-    plt.plot(x_plt, y_plt)
+    plt.plot(x_plt, y_package_id_plt, label="package_id")
+    plt.plot(x_plt, y_core_0_plt, label="core_0")
+    plt.plot(x_plt, y_core_1_plt, label="core_1")
+    plt.legend()
 
 ani = FuncAnimation(plt.gcf(), animate, interval=1000)
 
