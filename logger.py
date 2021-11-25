4f4b69f67abbaccf293d3a58e16a35a6c77b0eb3
Sriram-bb63 <invadersriram2002@gmail.com>
2021-11-25 13:10:57 +0530

    auto run logger and graph

---
+++ logger.py
@@ -0,0 +1,56 @@
+import csv
+import psutil
+import time
+
+print(f"[INFO] Start {time.ctime()[11:19]}")
+
+print("[INFO] Press Ctrl+c to stop")
+
+fieldnames = ["time", "package_id", "core_0", "core_1", "ram", "fan"]
+
+with open("temp_log.csv", "w") as f:
+    csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+while True:
+
+	try:
+		d = psutil.sensors_temperatures()
+		temperature_package_id = d["coretemp"][0][1]
+		temperature_core_0 = d["coretemp"][1][1]
+		temperature_core_1 = d["coretemp"][2][1]
+		ram = round(abs((psutil.virtual_memory().available * 100 / psutil.virtual_memory().total) - 100), 2)
+		fan = list(psutil.sensors_fans().values())[0][0][1]
+		current_time = time.ctime()[11:19]
+
+		with open("temp_log.csv", "a") as f:
+			csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+			info = {
+				"time": current_time,
+				"package_id": temperature_package_id,
+				"core_0": temperature_core_0,
+				"core_1": temperature_core_1,
+				"ram": ram,
+				"fan": fan
+			}
+			csv_writer.writerow(info)
+
+		print(f"Logged {time.ctime()[11:19]}")
+
+	except KeyboardInterrupt:
+		print(f"\n[INFO] Stop {time.ctime()[11:19]}")
+		break
+	
+	time.sleep(5)
+
+f.close()
+
+print("[INFO] Data saved as temp_log.csv")
+
+# TO DO
+# Full scale gui version?
+# Full scale cli version?
+# Name?
+# Full time logger from boot to shutdown
+# Save in html
+# Graph using js
--- main.py
+++ main.py
@@ -1,56 +1,13 @@
-import csv
-import psutil
-import time
+import os
 
-print(f"[INFO] Start {time.ctime()[11:19]}")
+live_graph = input("[INFO] Live graph (y/n): ")
+if live_graph == "y":
+    print("""
+    The logger (logger.py) and the graph (graph.py) are 2 separate processes run by main.py
+    Ending one will not end the other.
 
-print("[INFO] Press Ctrl+c to stop")
-
-fieldnames = ["time", "package_id", "core_0", "core_1", "ram", "fan"]
-
-with open("temp_log.csv", "w") as f:
-    csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
-    csv_writer.writeheader()
-
-while True:
-
-	try:
-		d = psutil.sensors_temperatures()
-		temperature_package_id = d["coretemp"][0][1]
-		temperature_core_0 = d["coretemp"][1][1]
-		temperature_core_1 = d["coretemp"][2][1]
-		ram = abs((psutil.virtual_memory().available * 100 / psutil.virtual_memory().total) - 100)
-		fan = list(psutil.sensors_fans().values())[0][0][1]
-		current_time = time.ctime()[11:19]
-
-		with open("temp_log.csv", "a") as f:
-			csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
-			info = {
-				"time": current_time,
-				"package_id": temperature_package_id,
-				"core_0": temperature_core_0,
-				"core_1": temperature_core_1,
-				"ram": ram,
-				"fan": fan
-			}
-			csv_writer.writerow(info)
-
-		print(f"Logged {time.ctime()[11:19]}")
-
-	except KeyboardInterrupt:
-		print(f"\n[INFO] Stop {time.ctime()[11:19]}")
-		break
-	
-	time.sleep(5)
-
-f.close()
-
-print("[INFO] Data saved as temp_log.csv")
-
-# TO DO
-# Full scale gui version?
-# Full scale cli version?
-# Name?
-# Full time logger from boot to shutdown
-# Save in html
-# Graph using js
+    ps: close the terminal and if it doesn't work, use htop
+    """)
+    os.system("python logger.py & python graph.py")
+else:
+    os.system("python logger.py")
