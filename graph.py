a1502497dc7b9f8307a24223876c534951c3fc6f
Sriram-bb63 <invadersriram2002@gmail.com>
2021-11-29 11:50:39 +0530

    logs based on no of cores

--- graph.py
+++ graph.py
@@ -6,14 +6,14 @@ import pandas as pd
 def animate(i):
     df = pd.read_csv("temp_log.csv")
     x = df["time"]
-    y_package_id = df["package_id"]
     y_core_0 = df["core_0"]
     y_core_1 = df["core_1"]
+    y_core_2 = df["core_2"]
     y_ram = df["ram"]
     plt.cla()
-    plt.plot(x, y_package_id, label="package_id")
     plt.plot(x, y_core_0, label="core_0")
     plt.plot(x, y_core_1, label="core_1")
+    plt.plot(x, y_core_2, label="core_2")
     plt.plot(x, y_ram, label="ram")
     plt.xticks(rotation="vertical")
     plt.xlabel("Time")
@@ -21,6 +21,8 @@ def animate(i):
     plt.grid()
     plt.tight_layout()
     plt.legend(loc="upper left")
+    ax = plt.gca()
+    ax.set_ylim(bottom=0, top=100)
 
 ani = FuncAnimation(plt.gcf(), animate, interval=5000)
 
--- logger.py
+++ logger.py
@@ -6,7 +6,13 @@ print(f"[INFO] Start {time.ctime()[11:19]}")
 
 print("[INFO] Press Ctrl+c to stop")
 
-fieldnames = ["time", "package_id", "core_0", "core_1", "ram", "fan"]
+fieldnames = []
+fieldnames.append("time")
+dic = psutil.sensors_temperatures()
+for i in range(len(dic["coretemp"])):
+	fieldnames.append(f"core_{i}")
+fieldnames.append("ram")
+fieldnames.append("fan")
 
 with open("temp_log.csv", "w") as f:
     csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
@@ -15,24 +21,34 @@ with open("temp_log.csv", "w") as f:
 while True:
 
 	try:
-		d = psutil.sensors_temperatures()
-		temperature_package_id = d["coretemp"][0][1]
-		temperature_core_0 = d["coretemp"][1][1]
-		temperature_core_1 = d["coretemp"][2][1]
+		temperatures = []
+		for i in range(len(dic["coretemp"])):
+			temperatures.append(dic["coretemp"][i][1])
 		ram = round(abs((psutil.virtual_memory().available * 100 / psutil.virtual_memory().total) - 100), 2)
 		fan = list(psutil.sensors_fans().values())[0][0][1]
 		current_time = time.ctime()[11:19]
-
-		with open("temp_log.csv", "a") as f:
-			csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
-			info = {
-				"time": current_time,
-				"package_id": temperature_package_id,
-				"core_0": temperature_core_0,
-				"core_1": temperature_core_1,
+		
+		info = dict()
+		info.update(
+			{
+				"time": current_time
+			}
+		)
+		for i in range(len(temperatures)):
+			info.update(
+				{
+					f"core_{i}": temperatures[i]
+				}
+			)
+		info.update(
+			{
 				"ram": ram,
 				"fan": fan
 			}
+		)
+
+		with open("temp_log.csv", "a") as f:
+			csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
 			csv_writer.writerow(info)
 
 		print(f"Logged {time.ctime()[11:19]}")
@@ -46,11 +62,3 @@ while True:
 f.close()
 
 print("[INFO] Data saved as temp_log.csv")
-
-# TO DO
-# Full scale gui version?
-# Full scale cli version?
-# Name?
-# Full time logger from boot to shutdown
-# Save in html
-# Graph using js
